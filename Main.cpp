ce99581848a7c047dfa39b347f43125d8fef74a3
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-03-14 20:30:54 +0200

    Update and rename README.md to Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <memory> // для std::shared_ptr
+
+class Item
+{
+public:
+	Item() { std::cout << "Item acquired\n"; }
+	~Item() { std::cout << "Item destroyed\n"; }
+};
+
+int main() {
+	std::shared_ptr<Item> ptr1 = std::make_shared<Item>();
+	{
+		std::shared_ptr<Item> ptr2 = ptr1;
+		std::cout << "Killing one shared pointer\n";
+	}
+
+	std::cout << "Killing another shared pointer\n";
+
+	return 0;
+}
+
+/*
+  виправити програму, щоб вона не видавала помилку
+*/
